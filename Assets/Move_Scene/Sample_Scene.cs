4eb6d77c23fc28f3b616403d58015b6e825f19c9
Nibosi <k22137kk@aitech.ac.jp>
2024-06-28 12:20:38 +0900

    add: Move_Scene_Script

---
+++ Assets/Move_Scene/Sample_Scene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float goal;
+    private float top_position_y = 0.0f;
+    void Start()
+    {
+        //Debug.Log("Sample Scene");
+        set_segment.top_position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        top_position_y = set_segment.top_position.y;
+        if (top_position_y >= goal)
+        {
+            //Debug.Log("Goal!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("End");
+        }
+    }
+}
