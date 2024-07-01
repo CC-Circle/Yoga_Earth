cf653538c61118ea6c272944312d2e54313cf21d
otakumi-bekon <otatakumi2004@gmail.com>
2024-07-01 11:57:40 +0900

    衝突判定

---
+++ Assets/OnCollision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    // Use this for initialization
+	void Start () {
+		
+	}
+ 
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log("作動中");
+    }
+    void OnTriggerEnter(Collider Collider)
+    {
+        // Debug.Log("接触したオブジェクト：" + gameObject.name);
+        // Debug.Log("接触されたオブジェクト：" + Collider.gameObject.name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("End");
+ 
+    }
+}
