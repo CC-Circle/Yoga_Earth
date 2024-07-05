cb317a2901417b57808062178bfbb60c20efd298
otakumi-bekon <otatakumi2004@gmail.com>
2024-07-05 10:50:59 +0900

    追加変更

--- Assets/enemy_script/RocketMovement.cs
+++ Assets/enemy_script/RocketMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketMove : MonoBehaviour
+public class RocketMovement : MonoBehaviour
 {
     // 目標のゲームオブジェクト
     // public Transform target;
