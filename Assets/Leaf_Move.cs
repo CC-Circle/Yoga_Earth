b52e1cbe2356c140aaf54bb22476d0afc99c01ec
Nibosi <k22137kk@aitech.ac.jp>
2024-07-05 15:39:03 +0900

    add:Bush

--- Assets/Leaf_Move.cs
+++ Assets/Leaf_Move.cs
@@ -18,14 +18,14 @@ public class Leaf_Move : MonoBehaviour
     {
         // set_segment クラスが正しく定義されていることを確認してください
         // ここでは set_segment.top_position を取得します
-        float top_position_x = set_segment.top_position.x * 0.6f - 1.2f;
-        if (top_position_x > 0.8f)
+        float top_position_x = set_segment.top_position.x * 0.9f - 1.1f;
+        if (top_position_x > 5.5f)
         {
-            top_position_x = 0.8f;
+            top_position_x = 5.5f;
         }
-        if (top_position_x < -3.0f)
+        if (top_position_x < -4.5f)
         {
-            top_position_x = -3.0f;
+            top_position_x = -4.5f;
         }
         float top_position_y = set_segment.top_position.y + 5.0f;
         Debug.Log("Top position - X: " + top_position_x + ", Y: " + top_position_y);
