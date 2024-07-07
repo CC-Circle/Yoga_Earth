9d271ef4621dab43506e5db6c941fcf4a202021b
Ryusan <x22037xx@aitech.ac.jp>
2024-07-07 20:57:07 +0900

    修正: 一連のplayで保存できるように

--- Assets/Score/Get_Score.cs
+++ Assets/Score/Get_Score.cs
@@ -9,36 +9,67 @@ using System;
 public class Get_Score : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject save_score;
+    public GameObject save_score; // Save_Scoreオブジェクト
 
     [SerializeField] public TextMeshProUGUI score_text; // Textオブジェクト
 
-    int[] score;
+    int[] score;//スコアを格納する配列
 
     void Start()
     {
         save_score =  GameObject.Find("Save_Score");
         // score_object = GameObject.Find("Text");
-        score = save_score.GetComponent<SaveScore>().GetTopScores();
+        // score = save_score.GetComponent<SaveScore>().GetTopScores();
         // Debug.Log("score");
     }
 
     // Update is called once per frame
     void Update()
     {
+        score = save_score.GetComponent<SaveScore>().GetTopScores();
         // デバッグ用
         // Debug.Log("score");
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     Array.Resize(ref score, i+1);
-        //     score[i] = i+1;
-        //     Debug.Log(i + 1 + "th: " + score[i]);
-        // }
-        // テキストの表示を入れ替える
-        score_text.text = "1st: " + score[0] + "\n" +
-                          "2nd: " + score[1] + "\n" +
-                          "3rd: " + score[2] + "\n" +
-                          "4th: " + score[3] + "\n" +
-                          "5th: " + score[4] + "\n";
+        int count = 1;
+        foreach (var score in score)
+        {
+            Debug.Log(score);
+            if (count == 1)
+            {
+                score_text.text = count + "st: " + score + "\n";
+                count++;
+                continue;
+            }
+            if (count == 2)
+            {
+                score_text.text += count + "nd: " + score + "\n";
+                count++;
+                continue;
+            }
+            if (count == 3)
+            {
+                score_text.text += count + "rd: " + score + "\n";
+                count++;
+                continue;
+            }
+            score_text.text += count + "th: " + score + "\n";
+            count++;
+        }
+        if(count == 2)
+        {
+            score_text.text += 2 + "th: " + 0 + "\n"+
+            3 + "nd: " + 0 + "\n"+ 4 + "th: " + 0 + "\n"+ 5 + "th: " + 0 + "\n";
+        }
+        if(count == 3)
+        {
+            score_text.text += 3 + "nd: " + 0 + "\n"+ 4 + "th: " + 0 + "\n"+ 5 + "th: " + 0 + "\n";
+        }
+        if(count == 4)
+        {
+            score_text.text += 4 + "th: " + 0 + "\n"+ 5 + "th: " + 0 + "\n";
+        }
+        if(count == 5)
+        {
+            score_text.text += 5 + "th: " + 0 + "\n";
+        }
     }
 }
