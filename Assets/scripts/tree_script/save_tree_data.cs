6a28c6809a24a829fb217ed2e85a6c8cecb6c7e2
ki43 <k22103kk@aitech.ac.jp>
2024-09-10 14:56:31 +0900

    3d対応

--- Assets/scripts/tree_script/save_tree_data.cs
+++ Assets/scripts/tree_script/save_tree_data.cs
@@ -10,19 +10,24 @@ public class save_tree_data : MonoBehaviour
     [SerializeField] GameObject cube_obj;
     [SerializeField] GameObject top_glass;
     public Transform parentTran;
+    public Terrain terrain;
 
-    [SerializeField] float moveSpeed = 100.0f; // メッシュの移動速度
-    [SerializeField] float growthSpeed = 2.0f; // 1秒あたりの成長量
-    [SerializeField] float growthLimit = 1.0f; // 最大成長高さ
-    [SerializeField] float growthAngle = 30.0f; // 成長の角度 (X軸正方向からの傾き角度)
-    [SerializeField] float left_limit = -2.0f;
-    [SerializeField] float right_limit = 2.0f;
-    [SerializeField] bool is_key = false;
+    //[SerializeField] float moveSpeed = 100.0f; // メッシュの移動速度
+    //[SerializeField] float growthSpeed = 2.0f; // 1秒あたりの成長量
+    //[SerializeField] float growthLimit = 1.0f; // 最大成長高さ
+    //[SerializeField] float growthAngle = 30.0f; // 成長の角度 (X軸正方向からの傾き角度)
+    //[SerializeField] float left_limit = -2.0f;
+    //[SerializeField] float right_limit = 2.0f;
+    //[SerializeField] bool is_key = false;
 
     private static float growthDirection = 0.0f; // X軸方向の成長方向 (0: 真上, 1: 正方向, -1: 負方向)
     int tree_count = 0;
+    float y_offset = -10f;
+    float z_offset = 40f;
 
     int[] offset_Arrays = { 1, 10, 20, 50 , -30, 6 , -20, 40, -15, -10};
+    int[] offset_Arrays_z = { 1, 10, 20, 50, -30, 6, -20, 40, -15, -10 };
+
 
     void Start()
     {
@@ -37,14 +42,23 @@ public class save_tree_data : MonoBehaviour
         {
             bool _ins_top = false;
 
+            float x_offs = item[0].x + offset_Arrays[tree_count];
+            float z_offs = item[0].z + offset_Arrays_z[tree_count];
+
+            // 高さを取得
+            float height = terrain.SampleHeight(new Vector3(x_offs, 0, z_offs));
+            height -= 4.7f;
+
+            Debug.Log(height);
+            
 
             for (int i = 0; i < item.Count; i++)
             {
-                GameObject obj = Instantiate(cube_obj, new Vector3(item[i].x + offset_Arrays[tree_count], item[i].y - 40f, item[i].z + 40f), Quaternion.identity);
+                GameObject obj = Instantiate(cube_obj, new Vector3(item[i].x + offset_Arrays[tree_count], item[i].y + height, item[i].z + offset_Arrays_z[tree_count]), Quaternion.identity);
                 obj.transform.SetParent(parentTran);
                 if (_ins_top == false)
                 {
-                    GameObject obj_glass = Instantiate(top_glass, new Vector3(item[item.Count - 1].x + offset_Arrays[tree_count], item[item.Count - 1].y - 40f, item[item.Count - 1].z + 40f), Quaternion.identity);
+                    GameObject obj_glass = Instantiate(top_glass, new Vector3(item[item.Count - 1].x + offset_Arrays[tree_count], item[item.Count - 1].y + height, item[item.Count - 1].z + offset_Arrays_z[tree_count]), Quaternion.identity);
                     _ins_top = true;
 
                     obj_glass.transform.SetParent(parentTran);
